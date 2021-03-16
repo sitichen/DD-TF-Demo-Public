079ab8a1395497975878cb22539336aee3724e3d
sitichen <57802943+sitichen@users.noreply.github.com>
2021-03-16 15:03:40 +1100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# DD-TF-Demo
---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    datadog = {
+      source  = "DataDog/datadog"
+      version = "2.13.0"
+    }
+  }
+
+  required_version = "~> 0.13"
+}
+
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  }
